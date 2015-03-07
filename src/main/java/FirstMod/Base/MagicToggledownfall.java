7150cb251f8cc26bcfb2c7a580c2adb521bcbd0b
RandomTestFive <randomtestfive@gmail.com>
2015-03-07 12:15:44 -0500

    added MagicToggledownfall
    Won't appear in-game yet.

---
+++ src/main/java/FirstMod/Base/MagicToggledownfall.java
@@ -0,0 +1,37 @@
+package FirstMod.Base;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class MagicToggledownfall extends Item
+{
+	public MagicToggledownfall()
+	{
+		this.setCreativeTab(derpTab.tabDerpCoin);
+		this.setMaxStackSize(1);
+	}
+	
+	 public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World,EntityPlayer par3EntityPlayer) 
+	 {
+		 System.out.println("toggledown");
+		 //par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 1F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+		 if(par3EntityPlayer.capabilities.isCreativeMode||par3EntityPlayer.inventory.consumeInventoryItem(Thingy.derpCoin))
+		 {
+			 if (par2World.isRemote)
+			 {
+				 if(par2World.isRaining())
+				 {
+					 par2World.setRainStrength(0);
+				 }
+				 else
+				 {
+					 par2World.setRainStrength(200);
+				 }
+			 }		 
+		 }
+		 return par1ItemStack;
+	 }
+	 
+}
