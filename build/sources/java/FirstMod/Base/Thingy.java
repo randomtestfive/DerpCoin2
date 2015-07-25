d5f0ba32ae511cdf92b66611b2fe8ca19c9c44e9
snydecor000 <snydecor000@students.centergrove.k12.in.us>
2015-07-25 19:44:29 -0400

    Fix Commit
    things gradlew

--- build/sources/java/FirstMod/Base/Thingy.java
+++ build/sources/java/FirstMod/Base/Thingy.java
@@ -38,7 +38,7 @@ public class Thingy
     public static final String MODID = "ThingyTest";
     public static final String VERSION = "0.4.7";
     
-    @SidedProxy(clientSide="FirstMod.Client.ClientProxy", serverSide="FirstMod.Server.ServerProxy")
+    @SidedProxy(clientSide="FirstMod.Client.ClientProxy", serverSide="CommonProxy")
     public static CommonProxy proxy;
     public static ClientProxy client;
     public static DerpCoin derpCoin;
@@ -68,7 +68,7 @@ public class Thingy
     	
     	derpPickaxe = new DerpPickaxe(derpMaterial);
 
-    	client.registerRenderers();
+    	//client.registerRenderers();
     	
 		magicItem = new MagicItem();
 
