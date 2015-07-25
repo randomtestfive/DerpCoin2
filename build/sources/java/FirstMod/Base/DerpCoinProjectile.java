f71b591895cd82cf35a4626e8395c76972be4692
snydecor000 <snydecor000@students.centergrove.k12.in.us>
2015-07-25 09:22:35 -0500

    Fixed Magic Item
    Still need a way to kill derpCoin entities when the world is being
    unloaded

--- build/sources/java/FirstMod/Base/DerpCoinProjectile.java
+++ build/sources/java/FirstMod/Base/DerpCoinProjectile.java
@@ -38,7 +38,8 @@ public class DerpCoinProjectile extends EntityThrowable
     		++ticksAlive;
     		if(ticksAlive >= 200)
     		{
-    			this.dropItem(Thingy.derpCoin, 1);
+    			this.entityDropItem(new ItemStack(Thingy.derpCoin), 0.0F);
+    			//this.dropItem(Thingy.derpCoin, 1);
 				//this.worldObj.spawnEntityInWorld(new EntityItem(this.worldObj, this.posX, this.posY, this.posZ, new ItemStack(Thingy.derpCoin)));
     			this.setDead();
     		}
